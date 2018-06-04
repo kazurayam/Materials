b335a46242e7521437e80d8bef7ed8e0e09074e1
URAYAMA,kazuaki <kazuaki.urayama@gmail.com>
2018-06-04 11:00:08 +0900

    factor out the CommandRunner class from ImageMagickVisualTestingDriver class

---
+++ src/test/groovy/com/kazurayam/webtestingresultstorage/CommandRunnerSpec.groovy
@@ -0,0 +1,33 @@
+package com.kazurayam.webtestingresultstorage
+
+import spock.lang.Specification
+
+class CommandRunnerSpec extends Specification {
+
+    // fields
+
+    // fixture methods
+    def setup() {}
+    def cleanup() {}
+    def setupSpec() {}
+    def cleanupSpec() {}
+
+    // feature methods
+    def testJavaVersion() {
+        when:
+        String[] args = ['java', '-version']
+        OutputStream out = new ByteArrayOutputStream()
+        OutputStream err = new ByteArrayOutputStream()
+        CommandRunner.runCommand(args, out, err)
+        def outString = out.toString('UTF-8')
+        def errString = err.toString('UTF-8')
+        System.out.println("${outString}")
+        System.err.println("${errString}")
+        then:
+        outString.length() == 0
+        errString.contains('java version')
+    }
+
+    // helper methods
+
+}
