34485624b00fe6cca5e8b4e45ef01069761eb8c3
URAYAMA,kazuaki <kazuaki.urayama@gmail.com>
2018-07-02 11:44:59 +0900

    changed Indexer#generate2() to be generate()

--- src/main/groovy/com/kazurayam/carmina/material/Indexer.groovy
+++ src/main/groovy/com/kazurayam/carmina/material/Indexer.groovy
@@ -7,7 +7,6 @@ import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 
 import groovy.json.JsonOutput
-import groovy.xml.MarkupBuilder
 
 class Indexer {
 
@@ -29,6 +28,12 @@ class Indexer {
         baseDir_ = baseDir
     }
 
+
+    /**
+     *
+     * @return
+     * @throws IOException
+     */
     Path makeIndex() throws IOException {
         RepositoryScanner scanner = new RepositoryScanner(baseDir_)
         scanner.scan()
@@ -40,121 +45,6 @@ class Indexer {
         return index
     }
 
-    Path makeIndex2() throws IOException {
-        RepositoryScanner scanner = new RepositoryScanner(baseDir_)
-        scanner.scan()
-        RepositoryRoot repoRoot = scanner.getRepositoryRoot()
-        Path index = baseDir_.resolve('index2.html')
-        OutputStream os = index.toFile().newOutputStream()
-        this.generate2(repoRoot, os)
-        logger_.info("generated ${index.toString()}")
-        return index
-    }
-
-    /**
-     * creates a HTML which displays a hierarchical tree structures of the Materials of the specified TSuiteResult.
-     *
-     * using Bootstrap-treeview (https://github.com/jonmiles/bootstrap-treeview)
-     *
-     * @param tSuiteResult
-     * @param os
-     * @throws IOException
-     */
-    void generate(RepositoryRoot repoRoot, OutputStream os) throws IOException {
-        def writer = new OutputStreamWriter(os, 'UTF-8')
-        def builder = new MarkupBuilder(writer)
-        builder.doubleQuotes = true
-        builder.html {
-            head {
-                meta('http-equiv':'X-UA-Compatible', content:'IE=edge')
-                title("${repoRoot.getBaseDir().getFileName().toString()}")
-                meta('charset':'utf-8')
-                meta('name':'description', 'content':'')
-                meta('name':'author', 'content':'')
-                meta('name':'viewport', 'content':'width=device-width, initial-scale=1')
-                link('rel':'stylesheet', 'href':'')
-                mkp.comment('''[if lt IE 9]
-<script src="//cdn.jsdelivr.net/html5shiv/3.7.2/html5shiv.min.js"></script>
-<script src="//cdnjs.cloudflare.com/ajax/libs/respond.js/1.4.2/respond.min.js"></script>
-<![endif]''')
-                link('rel':'shortcut icon', 'href':'')
-                link('href':'https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css',
-                        'rel':'stylesheet',
-                        'integrity':'sha384-WskhaSGFgHYWDcbwN70/dfYBj47jz9qbsMId/iRN3ewGhXQFZCSftd1LZCfmhktB',
-                        'crossorigin':'anonymous')
-                link('href':'https://cdnjs.cloudflare.com/ajax/libs/bootstrap-treeview/1.2.0/bootstrap-treeview.min.css',
-                    'rel':'stylesheet')
-            }
-            body() {
-                div('class':'container') {
-                    h3('Materials')
-                    div('id':'tree') { mkp.yield('') }
-                    div('id':'footer') { mkp.yield('') }
-                    div('id': 'modal-windows') { mkp.yield('') }
-                }
-
-                mkp.comment('SCRIPTS')
-                script('src':'https://code.jquery.com/jquery-3.3.1.slim.min.js',
-                    'integrity':'sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo',
-                    'crossorigin':'anonymous') {mkp.comment('')}
-                script('src':'https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js',
-                    'integrity':'sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49',
-                    'crossorigin':'anonymous') {mkp.comment('')}
-                script('src':'https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js',
-                    'integrity':'sha384-smHYKdLADwkXOn1EmN1qk/HfnUcbVRZyYmZ4qpPea6sjB/pTJ0euyQp0Mk8ck+5T',
-                    'crossorigin':'anonymous') {mkp.comment('')}
-                script('src': 'https://cdnjs.cloudflare.com/ajax/libs/bootstrap-treeview/1.2.0/bootstrap-treeview.min.js'
-                    ) {mkp.comment('')}
-                script('type':'text/javascript') {
-                    mkp.comment(
-'''
-function getTree() {
-    // Some logic to retrieve, or generate tree structure
-    var data = ''' + JsonOutput.prettyPrint(repoRoot.toBootstrapTreeviewData()) + ''';
-    return data;
-}
-$('#tree').treeview({
-    data: getTree(),
-    enableLinks: true,
-    levels: 3,
-    multiSelect: false,
-    showTags: true
-});
-''')
-                }
-                script('type':'text/javascript') {
-                    mkp.comment('''
-// insert modal windows
-$('#modal-windows').append($(` ''' + repoRoot.htmlFragmensOfMaterialsAsModal() + ''' `));
-
-// modify anchor tags in the treeview
-$(function() {
-    $('#tree a').each(function() {
-        if ($(this).attr('href') && $(this).attr('href') != '#') {
-            $(this).attr('data-toggle', 'modal');
-            $(this).attr('data-target', $(this).attr('href'));
-            $(this).attr('href', '#');
-        }
-    });
-
-    // reload the document on closing the Modal window
-    $('.modal').on('hidden.bs.modal', function () {
-        location.reload();
-    });
-
-});
-'''
-                    )
-                }
-            }
-        }
-        writer.flush()
-        writer.close()
-
-    }
-
-
-
 
 
     /**
@@ -163,7 +53,7 @@ $(function() {
      * @param os
      * @throws IOException
      */
-    void generate2(RepositoryRoot repoRoot, OutputStream os) throws IOException {
+    void generate(RepositoryRoot repoRoot, OutputStream os) throws IOException {
         def writer = new OutputStreamWriter(os, 'UTF-8')
         StringBuilder sb = new StringBuilder()
         sb.append('<html>'                                                                    + "\n")
