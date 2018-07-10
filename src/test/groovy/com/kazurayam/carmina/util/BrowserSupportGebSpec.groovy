a2f960e3a3a86ad853b4f6b27a3170fbc9f08f72
URAYAMA,kazuaki <kazuaki.urayama@gmail.com>
2018-07-10 17:49:50 +0900

    updates for Firefox case with BrowserSupportGebSpec

--- src/test/groovy/com/kazurayam/carmina/util/BrowserSupportGebSpec.groovy
+++ src/test/groovy/com/kazurayam/carmina/util/BrowserSupportGebSpec.groovy
@@ -4,8 +4,11 @@ import java.nio.charset.StandardCharsets
 import java.nio.file.Files
 import java.nio.file.Path
 import java.nio.file.Paths
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
 
 import org.openqa.selenium.Capabilities
+import org.openqa.selenium.Keys
 import org.openqa.selenium.remote.RemoteWebDriver
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
@@ -51,12 +54,17 @@ class BrowserSupportGebSpec extends GebSpec {
         // for debug
         //printFirefoxProfiles('testDownloadCsvFileAndGetItAsAPath')
         to ResultsFIFAWorldCup2018JapanPage
+        logger_.debug("${getTimestampOfNow()} #testDownloadCsvFileAndGetItAsAPath before waitFor anchorDownloadCsv")
         waitFor(10) {
             anchorDownloadCsv
         }
-        logger_.debug("#testDownloadCsvFileAndGetItAsAPath before anchorDownloadCsv.click")
-        anchorDownloadCsv.click()
-        logger_.debug("#testDownloadCsvFileAndGetItAsAPath after anchorDownloadCsv.click")
+        logger_.debug("${getTimestampOfNow()} #testDownloadCsvFileAndGetItAsAPath before anchorDownloadCsv.click")
+
+        anchorDownloadCsv.sendKeys(Keys.chord(Keys.ENTER))
+        // anchorDownloadCsv.click() is problematic. Invoking click() gets stuck in there and does not continue the execution till the timeout.
+        // see https://stackoverflow.com/questions/46939451/timeout-when-using-click-webdriver-selenium-function-python
+
+        logger_.debug("${getTimestampOfNow()} #testDownloadCsvFileAndGetItAsAPath after anchorDownloadCsv.click")
 
         //Path downloadedFile = BrowserSupport.waitForFileDownloaded(downloadsDir_, 3000, 10000)
 
@@ -78,5 +86,11 @@ class BrowserSupportGebSpec extends GebSpec {
             count += 1
             logger_.debug("#${methodName} FirefoxProfile:${count} ${line}")
         }
+    }
+
+    private String getTimestampOfNow() {
+        LocalDateTime ldt = LocalDateTime.now()
+        //return DateTimeFormatter.ofPattern("yyyy/MM/dd'T'HH:mm:ss.SSS").format(ldt)
+        return DateTimeFormatter.ISO_LOCAL_DATE_TIME.format(ldt)
     }
 }
