ff6b36bd977ac3ad3e481b0e9d86dc1496d6f67f
kazurayam <kazuaki.urayama@gmail.com>
2019-06-22 06:39:13 +0900

    removed the com.kazurayam.imagedifference package

--- src/main/groovy/com/kazurayam/imagedifference/package-info.groovy
+++
@@ -1,8 +0,0 @@
-/*
- * This package is here for backward compatibility for older version of "Visual Testing in Katalon Studio" project.
- * 
- * The com.kazurayam.imagedifference.ImageDifference class is an alias to 
- * the com.kazurayam.materials.imagedifference.ImageDifference class.
- * 
- */
-package com.kazurayam.imagedifference;
