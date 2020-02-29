cc6df7c1212cbbcaeec3767433c92d64129afab8
kazurayam <kazuaki.urayama@gmail.com>
2020-03-01 08:26:30 +0900

    add serializeAsMarkdown() to MaterialMetadataBundle class; add serializeAsTSV() to MaterialMetadataBundle class

--- src/main/groovy/com/kazurayam/materials/impl/MaterialRepositoryImpl.groovy
+++ src/main/groovy/com/kazurayam/materials/impl/MaterialRepositoryImpl.groovy
@@ -812,6 +812,7 @@ final class MaterialRepositoryImpl implements MaterialRepository {
     /**
      * @return Path of material-metadata-bundl.json file under the directory of the TSuiteResult
      */
+    @Override
     Path locateMaterialMetadataBundle(TSuiteResult tSuiteResult) {
         return tSuiteResult.getTSuiteTimestampDirectory().resolve(
                 MaterialMetadataBundle.SERIALIZED_FILE_NAME)
