9d2d06200efb278718668cba16c76c132f163aa6
kazurayam <kazuaki.matsuhashi@quick.jp>
2020-07-02 17:15:50 +0900

    updates

--- src/main/groovy/com/kazurayam/materials/view/RepositoryVisitorGeneratingHtmlDivsAsModalBase.groovy
+++ src/main/groovy/com/kazurayam/materials/view/RepositoryVisitorGeneratingHtmlDivsAsModalBase.groovy
@@ -172,10 +172,10 @@ abstract class RepositoryVisitorGeneratingHtmlDivsAsModalBase
                 generateImgTags(mate)
                 break
             case FileType.CSV:
+            case FileType.HTML:
                 mkbuilder_.pre(['class':'pre-scrollable'], mate.getPath().toFile().getText('UTF-8'))
                 break
             case FileType.TXT:
-            case FileType.HTML:
                 mkbuilder_.div(['style':'height:350px;overflow:auto;']) {
                     File file = mate.getPath().toFile()
                     file.readLines('UTF-8').each { line ->
